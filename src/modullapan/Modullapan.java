cb01f498f21a19e3525f6aeb842592b2da7b59b0
Praktikum <Praktikum@Lenovo-PC>
2015-04-07 07:56:05 +0700

    new commit

---
+++ src/modullapan/Modullapan.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package modullapan;
+
+/**
+ *
+ * @author Praktikum
+ */
+public class Modullapan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Sulistianto Anggi Pratama - 6306130036");
+        System.out.println("Saya sangat menyukai praktikum RPL modul 8 ini");
+    }
+    
+}
